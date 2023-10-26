ce5d2f08ac2749ce70eb3951a24fd686ac98c956
unknown <alok@aladinn.com>
2023-10-26 17:52:22 +0530

    first

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
     domains: ["images.unsplash.com" ,'vritti-bucket.s3.amazonaws.com'],
       
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+},
 };
 
 module.exports = nextConfig
